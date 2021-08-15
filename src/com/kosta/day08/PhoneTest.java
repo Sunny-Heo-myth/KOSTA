0383ec02290931e636a3f3181341d9b1b3ea4f20
Astronaut <hsymyth@gmail.com>
2021-08-15 23:21:20 +0900

    Algorithm practice

--- src/com/kosta/day08/PhoneTest.java
+++ src/com/kosta/day08/PhoneTest.java
@@ -3,8 +3,9 @@ package com.kosta.day08;
 public class PhoneTest {
 
 	public static void main(String[] args) {
-		new Smartphone("Sunny", "iphone7", 0);
-
+		Phone a = new Smartphone("Sunny", "iphone7", 0);
+		
+		a.internetSearch();
 	}
 
 }
